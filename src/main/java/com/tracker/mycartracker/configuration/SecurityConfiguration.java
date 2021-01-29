5bdc4500e9faab449586e91e2b67dcaedcb0a5ec
boghy933 <boghy933@gmail.com>
2021-01-29 17:37:28 +0100

    Add: disable sessions (session stateless)

--- src/main/java/com/tracker/mycartracker/configuration/SecurityConfiguration.java
+++ src/main/java/com/tracker/mycartracker/configuration/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -22,8 +23,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        // http.formLogin(); - Used for debug
-
+        http.formLogin(); // - Used for debug
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http
                 .csrf().disable()
                 .authorizeRequests()
